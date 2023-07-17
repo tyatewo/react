92619d03514daf84565447f4062833ef75501f3a
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-17 08:14:34 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <h1>タグを用いて、「Hello World」と表示してください */}
+        <h1>Hello World</h1>
+        <p>一緒にReactを学びましょう！</p>
+        {/* <p>タグを用いて、「一緒にReactを学びましょう！」と表示してください */}
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
